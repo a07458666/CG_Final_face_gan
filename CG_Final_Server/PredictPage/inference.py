609fec76f17e562562d1435d7d00eaf90ee21f68
andy su <andysu.cs10@nycu.edu.tw>
2022-01-02 14:03:10 +0800

    merge

---
+++ CG_Final_Server/PredictPage/inference.py
@@ -0,0 +1,24 @@
+import torch 
+import cv2
+import torchvision.transforms as transforms
+import numpy as np
+
+
+def converFace(model, image):
+    transform = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize((256, 256)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+    data = {"A_paths":"", "B_paths": ""}
+
+    data['A'] = torch.unsqueeze(transform(image), 0)
+    data['B'] = torch.unsqueeze(transform(image), 0)
+    model.set_input(data)  # unpack data from data loader
+    model.test()           # run inference
+    visuals = model.get_current_visuals()  # get image results
+    fake = visuals['fake_B'].squeeze(0).cpu().numpy()
+    fake = np.swapaxes(fake,0,1)
+    fake = np.swapaxes(fake,1,2)
+    return fake
